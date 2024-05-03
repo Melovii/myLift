a17dc4f3290aa8c547e417b374457a93d4648f35
Melovii <145908254+Melovii@users.noreply.github.com>
2024-05-03 12:26:59 +0300

    added scanner

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class Main
 {
     public static void main (String[] args)
     {
         System.out.println("Hello Vroosh and Melovi!");
+
+        Scanner input = new Scanner(System.in);
+        int value = input.nextInt();
+        System.out.println(value);
     }
 }
