f76b91af38c8331e69a034cf33a67b2c21a6846b
Melovii <145908254+Melovii@users.noreply.github.com>
2024-05-04 12:56:19 +0300

    Added message

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,17 @@ public class Main
     {
         System.out.println("Hello Vroosh and Melovi!");
 
+        System.out.print("Enter a number: ");
+
         Scanner input = new Scanner(System.in);
         int value = input.nextInt();
         System.out.println(value);
+
+        if (value <= 4) {
+            System.out.println("Number is less than or equal to " +value);
+        }
+        else {
+            System.out.println("Number is greater than " +value);
+        }
     }
 }
