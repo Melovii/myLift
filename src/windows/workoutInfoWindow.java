9e0adb17ec8b7586a03fe69244a4132608821e6c
Melovii <145908254+Melovii@users.noreply.github.com>
2024-05-21 12:42:35 +0300

    made infoWindow and switched cancelB/saveB from addWorkoutWindow

--- src/windows/workoutInfoWindow.java
+++ src/windows/workoutInfoWindow.java
@@ -100,7 +100,7 @@ public class workoutInfoWindow extends JFrame {
         this.setSize(520, 300);
 
         this.getContentPane().setBackground(new Color(44, 43, 64));
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
         this.setResizable(false);
         this.setLocationRelativeTo(null);
         this.setTitle("Add Workout");
